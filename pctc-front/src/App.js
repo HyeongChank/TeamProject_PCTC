b4dc8d458d90de932cdae89328ed7ce9d2929174
ChrisEuristic <eanair@kakao.com>
2023-04-28 17:27:16 +0900

    로그인 후의 signin.js 개ëÇ±âront) -> js #8
    로그인, 회원가입, Mypage용 팝업창 구현
    로그인 페이지 구현
    로그인 후 signin 컴포넌트 구현

--- pctc-front/src/App.js
+++ pctc-front/src/App.js
@@ -1,20 +1,25 @@
 /* Level 0 */
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+import { useState } from 'react'
 
 import './App.css';
 
 import Apps from './Apps';
-import Login from './component/Login';
 import Home from './component/Home';
 
 function App() {
+  /* 로그인 상태에 따른 페이지 개발을 위해 임시로 설정한 변수입니다. */
+  const [isLogin, setIsLogin] = useState(false);
+  /* 로그인 상태에 따른 페이지 개발을 위해 임시로 설정한 변수입니다. */
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' Component={Home} />
-          <Route path='/signin' Component={Login} />
+          <Route path='/' element={<Home isLogin={isLogin} setIsLogin={setIsLogin} />} />
+          {/* <Route path='/login' Component={Login} />
+          <Route path='/join' Component={Join} /> */}
         </Routes>
       </BrowserRouter>
     </>
--- pctc-front/src/component/TitleView.js
+++ pctc-front/src/component/TitleView.js
@@ -5,12 +5,12 @@ import './componentstyle.css'
 import logo from '../assets/logo.png'
 import { Link } from "react-router-dom";
 
-const TitleView = () => {
+const TitleView = (props) => {
   return(
     <div className='titleview'>
       <Link to='/'><img className="titleview-logo" src={logo} alt="logo" /></Link>
       <div style={{'fontSize': '2em', fontWeight:'bold', textAlign: 'center'}}>TitleView Area</div>
-      <Signin />
+      <Signin isLogin={props.isLogin} setIsLogin={props.setIsLogin} />
     </div>
   )
 }
