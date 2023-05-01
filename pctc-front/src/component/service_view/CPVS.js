6798765b64f3616c726bb8cd79a832c0c09d8989
ChrisEuristic <eanair@kakao.com>
2023-05-01 19:11:55 +0900

    지도 그리고 chart와 결합(Front) -> js #13
    chart.js 대신 CSS로 간단하게 표현.
    실제 반투명 색상타원으로 표시할거라면 css 표현으로 충분할 것으로 봄.
    추후 요구사항에 따라 반영 고려.

--- pctc-front/src/component/service_view/CPVS.js
+++ pctc-front/src/component/service_view/CPVS.js
@@ -1,31 +1,48 @@
 /* Level 3 */
 /* Congestion prediction and visualization service */
 /* 혼잡도 예측 및 시각화 서비스 */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BarChart from './component/BarChart';
 import './serviceview.css'
 import { UserData } from './component/Data';
 
+const { kakao } = window;
 
 const CPVS = () => {
-  
+
   const [userData, setUserData] = useState({
     labels: UserData.map((x) => x.year),
     datasets: [{
       label: "Users Gained",
       data: UserData.map((x) => x.userGain),
-    },{
+    }, {
       label: "Users Gained2",
       data: UserData.map((x) => x.userLost),
     }]
   });
 
-  return(
+  useEffect(() => {
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(35.106, 129.08),
+      level: 4,
+      draggable: false,
+    };
+
+    var map = new kakao.maps.Map(container, options);
+  }, []);
+
+  return (
     <div className='cpvs'>
-      <div style={{'fontSize': '2em', fontWeight:'bold', textAlign: 'center'}}>CPVS Area</div>
-      <div style={{width: '700px', height:'700px'}}>
-        <BarChart chartData={userData} />
+      {/* <div style={{ 'fontSize': '2em', fontWeight: 'bold', textAlign: 'center' }}>CPVS Area</div> */}
+      <div id="map" style={{ width: '100%', height: '100%' }}>
+        <div id="area-1"></div>
+        <div id="area-2"></div>
+        <div id="area-3"></div>
       </div>
+      {/* <div style={{ width: '700px', height: '700px' }}>
+        <BarChart chartData={userData} />
+      </div> */}
     </div>
   )
 }
