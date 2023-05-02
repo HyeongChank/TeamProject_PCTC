83d62a432cbe1621ddf4e00635b39cf0eb31e92c
ChrisEuristic <eanair@kakao.com>
2023-05-02 10:36:11 +0900

    약어 텍스트 읽기 편하게 수정

--- pctc-front/src/component/service_view/SADRS.js
+++ pctc-front/src/component/service_view/SADRS.js
@@ -8,6 +8,7 @@ const SADRS = () => {
   return(
     <div className='sadrs'>
       <div style={{'fontSize': '2em', fontWeight:'bold', textAlign: 'center'}}>SADRS Area</div>
+      <div style={{'fontSize': '2em', fontWeight:'bold', textAlign: 'center'}}>선박 입항일자 추천 서비스</div>
     </div>
   )
 }
